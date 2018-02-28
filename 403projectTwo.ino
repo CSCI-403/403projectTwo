1f7ee2706dc24e31692cc533d8a9eaed186b3c5d
Chelsea Huff <novamarie1123@gmail.com>
2018-02-27 22:06:10 -0500

    Update and rename getTime.ino to 403projectTwo.ino

--- 403projectTwo.ino
+++ 403projectTwo.ino
@@ -1,12 +1,18 @@
+
 #include <WiFi.h>
 #include "time.h"
 
-const char* ssid       = "YOUR_SSID";
-const char* password   = "YOUR_PASS";
+const char* ssid       = "EXAMPLE_SSID";
+const char* password   = "EXAMPLE_PASSWORD";
 
 void setup()
 {
   Serial.begin(115200);
+  /*
+   create two tasks;
+    one to update the internal time value once every hour
+    and another to output time once every 10 miliseconds
+  */
 
   xTaskCreate(
     updateTimeTask, /* Task function. */
@@ -34,15 +40,11 @@ void setup()
   Serial.println(" CONNECTED");
 
   const char* ntpServer = "pool.ntp.org";
-  const long  gmtOffset_sec = 3600;
-  const int   daylightOffset_sec = 3600;
+  const long  gmtOffset_sec = 3600 * (-5);
+  const int   daylightOffset_sec = 0;
   
   //init and get the time
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-
-  //disconnect WiFi as it's no longer needed
-  WiFi.disconnect(true);
-  WiFi.mode(WIFI_OFF);
 }
 
 void loop()
@@ -50,10 +52,11 @@ void loop()
 
 }
 
+//get time once every hour; runs in an infinite loop
 void updateTimeTask( void * parameter){
   const char* ntpServer = "pool.ntp.org";
-  const long  gmtOffset_sec = 3600;
-  const int   daylightOffset_sec = 3600;
+  const long  gmtOffset_sec = 3600 * (-5);
+  const int   daylightOffset_sec = 0;
 
   for(;;){
     configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
@@ -63,17 +66,16 @@ void updateTimeTask( void * parameter){
   vTaskDelete( NULL ); 
 }
 
+//use the internally stored time and output it to the serial channel; runs in an infinite loop
 void printTimeTask( void * parameter){
   for(;;){
-    struct tm* timeinfo;
-    time_t timer;
-    char buffer[26];
-
-    time(&timer);
-    timeinfo = localtime(&timer);
-
-    
-    strftime(buffer, 26, "%Y/%m/%d %H:%M:%S", timeinfo);
+    struct tm timeinfo;
+    if(!getLocalTime(&timeinfo)){
+      Serial.println("Failed to obtain time");
+      return;
+    }
+    Serial.println(&timeinfo, "%Y/%m/%d %H:%M:%S");
+    delay(10);
   }
 
   vTaskDelete( NULL );
